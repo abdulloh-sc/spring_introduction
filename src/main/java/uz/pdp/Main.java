e5fc12f6a54c827c5df8f96490308cf40a664179
abdulloh7111 <abdullohjon101@gmail.com>
2025-12-25 14:52:28 +0500

    xml_config

--- src/main/java/uz/pdp/Main.java
+++ src/main/java/uz/pdp/Main.java
@@ -1,16 +1,15 @@
 package uz.pdp;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import uz.pdp.config.MyConfig;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
 
-        AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(MyConfig.class); /// config class orqali context hosil qilish
-//        MyBean myBean = context.getBean(MyBean.class); /// contextdan myBean ni olish
-//        myBean.hi();
-        MyBean2 myBean2 = context.getBean(MyBean2.class);
-        myBean2.getMyBean().hi();
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("ioc-settings.xml"); /// config class orqali context hosil qilish
+        MyBean myBean = context.getBean(MyBean.class); /// contextdan myBean ni olish
+        myBean.hi();
+//        MyBean2 myBean2 = context.getBean(MyBean2.class);
+//        myBean2.getMyBean().hi();
     }
 }
