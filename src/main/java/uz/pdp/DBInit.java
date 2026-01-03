d3e578f8b1c4eebc4092a4c61686889c1c2b0cdd
abdulloh7111 <abdullohjon101@gmail.com>
2026-01-03 19:55:57 +0500

    conditional bean

---
+++ src/main/java/uz/pdp/DBInit.java
@@ -0,0 +1,80 @@
+package uz.pdp;
+
+public class DBInit {
+
+    private void init() {
+        System.out.println("DBInit init method called");
+    }
+
+    private void destroy() {
+        System.out.println("DBInit destroy method called");
+    }
+
+//    private String url;
+//    private String db;
+//    private String user;
+//    private String password;
+//    private String driver;
+//
+//    public DBInit(String url, String db, String user, String password, String driver) {
+//        this.url = url;
+//        this.db = db;
+//        this.user = user;
+//        this.password = password;
+//        this.driver = driver;
+//    }
+//
+//    public DBInit() {
+//    }
+//
+//    public String getUrl() {
+//        return url;
+//    }
+//
+//    public void setUrl(String url) {
+//        this.url = url;
+//    }
+//
+//    public String getDb() {
+//        return db;
+//    }
+//
+//    public void setDb(String db) {
+//        this.db = db;
+//    }
+//
+//    public String getUser() {
+//        return user;
+//    }
+//
+//    public void setUser(String user) {
+//        this.user = user;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getDriver() {
+//        return driver;
+//    }
+//
+//    public void setDriver(String driver) {
+//        this.driver = driver;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "DBInit{" +
+//                "url='" + url + '\'' +
+//                ", db='" + db + '\'' +
+//                ", user='" + user + '\'' +
+//                ", password='" + password + '\'' +
+//                ", driver='" + driver + '\'' +
+//                '}';
+//    }
+}
--- src/main/java/uz/pdp/config/MainMyConfig.java
+++ src/main/java/uz/pdp/config/MainMyConfig.java
@@ -1,11 +1,18 @@
 package uz.pdp.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.*;
+import uz.pdp.DBIinitCondition;
+import uz.pdp.DBInit;
 
 @Import({MyConfig.class,MyConfig2.class})
 @Configuration
 @ImportResource(locations = "classpath:ioc-settings.xml")
+@PropertySource("classpath:application.properties")
 public class MainMyConfig {
+
+    @Bean(name = "dbInitBean", initMethod = "init", destroyMethod = "destroy")
+    @Conditional(DBIinitCondition.class)
+    public DBInit dbInit(){
+        return new DBInit();
+    }
 }
